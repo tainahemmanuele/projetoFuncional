bd54d169164bf730cd17fc173bd92e10249ede92
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-11-16 16:59:21 -0300

    Atualizacao no parser. Adicao de arquivo main

---
+++ haskell/Parser.hs
@@ -0,0 +1,21 @@
+module JsonParser
+(
+    getJSON,
+    getTransations
+) where
+
+import qualified Data.ByteString.Lazy as B  
+import Data.Aeson
+import Data.Maybe
+import Tipos
+
+jsonFile :: FilePath
+jsonFile = "dados.json"
+
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+
+getTransations :: IO [Transacao]
+getTransations = do
+    transations <- (decode <$> getJSON) :: IO (Maybe [Transacao])
+    return (fromJust transations)
