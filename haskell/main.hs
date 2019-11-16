fa70b92b1aa705b9bb8cf9e2b7258d726d6ce209
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-11-16 20:42:22 -0300

    documentacao de uso

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,3 +1,9 @@
 import Parser
 import DataTypes
 
+--https://stackoverflow.com/questions/46820379/how-to-use-parsers-from-aeson-with-io 
+-- Para entender pq usa IO para receber o dado da entrada
+--https://github.com/bos/aeson/blob/master/Data/Aeson.hs - Documentacao do que fazer oficial
+
+
+
