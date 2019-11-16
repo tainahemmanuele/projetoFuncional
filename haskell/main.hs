3386b748a7965cf0cb5af0a7c60f39f6aa8c639f
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-11-16 20:44:41 -0300

    documentacao de uso - exemplos

--- haskell/main.hs
+++ haskell/main.hs
@@ -4,6 +4,9 @@ import DataTypes
 --https://stackoverflow.com/questions/46820379/how-to-use-parsers-from-aeson-with-io 
 -- Para entender pq usa IO para receber o dado da entrada
 --https://github.com/bos/aeson/blob/master/Data/Aeson.hs - Documentacao do que fazer oficial
+-- Mais exemplos de uso:
+-- https://gist.github.com/nbogie/985645/a4ee72325b830e9bd7567a21dd05e691f4223fd9
+-- https://github.com/seek-oss/serverless-haskell/blob/master/src/AWSLambda/Handler.hs
 
 
 
