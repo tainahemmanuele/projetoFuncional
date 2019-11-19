6e13471499bf953d0927d9bf9a0de204bb3f5442
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-11-18 23:58:35 -0300

    Mais alguns testes

--- haskell/Tests.hs
+++ haskell/Tests.hs
@@ -7,13 +7,10 @@ import ParserTest
 
 -- Roda usando runTestTT nomedaFuncaoTeste
 --Compila Tests.hs antes
-
+-- OBS: Base de dados usada para o teste: dadosTests.json . Essa base é pequena, apenas para mostrar o funcionamento dos metodos
 main = do
    testFilterYear
-   testIsCredit
-   testIsBalance
-   testIsCreditOrDebit
-   testSumValues
+
 
 --testFilterYear :: ([Transacao] -> Assertion) -> IO b0
 testFilterYear = 
@@ -21,6 +18,11 @@ testFilterYear =
                 assertEqual "Filtra o ano de 2017" ([db !! 0 ] ++ [db !! 1] ++ [db !! 2]) (filterByYear  db  2017)
                 assertEqual "Filtra o ano de 2018 (Criado so para testes esses dados)" ([db !! 3] ++ [db !! 4]) (filterByYear  db  2018))
 
+testFilterByYearMonth = 
+   TestCase (do db <- getTransactionsTest 
+                assertEqual "Filtra o ano de 2016 e o mes 2" ([db !! 5 ] ++ [db !! 6] ++ [db !! 7] ++ [db !! 8]) (filterByYearMonth  db  2016 2))
+
+                
 testIsCredit = 
    TestCase (do db <- getTransactionsTest 
                 assertEqual "É credito #1?" False (isCredit (db !! 0))
@@ -48,13 +50,30 @@ testIsCreditOrDebit =
 
 testIsCreditOrDebitOrBalance = 
    TestCase (do db <- getTransactionsTest 
-                assertEqual "É credito ou debito ou balanco #1?" True (isCreditOrDebitOrBalancet  (db !! 0))
+                assertEqual "É credito ou debito ou balanco #1?" True (isCreditOrDebitOrBalance  (db !! 0))
                 assertEqual "É credito ou debito ou balanco #2?" True (isCreditOrDebitOrBalance  (db !! 1))
-                assertEqual "É credito ou ou balanco #3?" True (isCreditOrDebitOrBalance  (db !! 3))
+                assertEqual "É credito ou ou balanco #3?" True (isCreditOrDebitOrBalance  (db !! 3)))
 
 testSumValues = 
    TestCase (do db <- getTransactionsTest 
-                assertEqual "Soma dos valores" 81589.56 (sumValues db))
+                assertEqual "Soma dos valores" 81589.56000000001 (sumValues db))
 
+testSumCreditsByYearMonth = 
+   TestCase (do db <- getTransactionsTest 
+                assertEqual "Calcula os creditos  do ano de 2016 e o mes 2" 1152.11 (sumCreditsByYearMonth  db  2016 2)
+                assertEqual "Calcula os creditos  do ano de 2018 e o mes 2" 1152.11 (sumCreditsByYearMonth  db  2018 2)
+                assertEqual "Calcula os creditos  do ano de 2017 e o mes 2" 0.0 (sumCreditsByYearMonth  db  2017 2))
 
-                
+testSumDebitByYearMonth = 
+   TestCase (do db <- getTransactionsTest 
+                assertEqual "Calcula os debitos  do ano de 2016 e o mes 2" (-45.06999999999999) (sumDebitByYearMonth  db  2016 2)
+                assertEqual "Calcula os debitos  do ano de 2018 e o mes 2" (-50.0) (sumDebitByYearMonth  db  2018 2))
+
+testSumSobra = 
+   TestCase (do db <- getTransactionsTest 
+                assertEqual "Calcula a sobra(receitas - despesas)  do ano de 2018 e o mes 2" 1102.11 (sumSobra  db  2018 2))
+
+testFinalBalanceInMonth = 
+   TestCase (do db <- getTransactionsTest 
+                assertEqual "Calcula o saldo final  do ano de 2016 e o mes 2" 1107.0399999999997 (finalBalanceInMonth  db  2016 2)
+                assertEqual "Calcula o saldo final  do ano de 2018 e o mes 2" 1102.11 (finalBalanceInMonth  db  2018 2))
